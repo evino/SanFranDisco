4245c60d614505beaf47bd8045d3c831bc7d87ca
Evin <evinodisho@gmail.com>
2026-03-03 21:43:13 -0800

    getting temps for 24 hour window

--- weather.py
+++ weather.py
@@ -8,33 +8,59 @@ noaa_url = "https://api.weather.gov/gridpoints/MTR/85,98/forecast/hourly"
 tz = timezone(timedelta(hours=-8))
 dt = datetime.now(tz)
 
+
+# Get NWS metadata
 def GetWeatherData(url) -> dict:
     data = requests.get(url).json()
     # print(json.dumps(data, indent=4))
     return data
 
-def DateFilter(StartDate, EndDate):
-    print(date)
+# Returns number of hours left in current day
+def HoursLeftInDay(dt=None):
+    if dt is None:
+        dt = datetime.now()
+    current_hour = dt.replace(minute=0, second=0, microsecond=0)
+    midnight = (current_hour + timedelta(days=1)).replace(hour=0)
+    return int((midnight - current_hour).total_seconds() // 3600)
 
-def GetTemps(WeatherData):
-    periods = WeatherData["properties"]["periods"]
-    high_temp = max(p["temperature"] for p in periods)
-    for p in periods:
-        print(p["temperature"])
+"""
+Returns highest temperature for a 24-hour window based off a start period
+"""
+def GetTemps(WeatherData, startPeriod=0) -> float:
+    startPeriod += 1
+    endPeriod = startPeriod + 24
 
-    print(f"High temp of the day is: {high_temp}")
+    # Grab each period for 24 hour window based off startPeriod
+    periods = WeatherData["properties"]["periods"][startPeriod:endPeriod]
+    high_temp = max(float(p["temperature"]) for p in periods)
+
+
+    # For debug
+    # print(json.dumps(periods, indent=4))
+    # for p in periods:
+    #     print(p["temperature"])
+
+    print(f"High temp tomorrow ({periods[0]["startTime"]}) is: {high_temp}")
     # print(json.dumps(periods[0]["temperature"], indent=4))
+
+    return high_temp
+
 
 def main():
     noaa_data = GetWeatherData(noaa_url)
-    GetTemps(noaa_data)
+    # print(json.dumps(noaa_data, indent=4))
+
+    startPeriod = HoursLeftInDay()
+    GetTemps(noaa_data, startPeriod)
 
     now = dt  # Current date
     tomorrow = now.date() + timedelta(days=1)
     start = datetime.combine(tomorrow, time.min, tzinfo=tz).isoformat()
     end = datetime.combine(tomorrow + timedelta(days=1), time.min, tzinfo=tz).isoformat()
-    print(start)
-    print(end)
+    
+        
+    # print(start)
+    # print(end)
 
     # DateFilter()
 
