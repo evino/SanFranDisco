2e0bb837a930385484c26d66172a5fb26fa8c464
Evin <evinodisho@gmail.com>
2026-03-04 22:44:40 -0800

    GetBrackets() written

--- series.py
+++ series.py
@@ -9,7 +9,25 @@ url = f"https://api.elections.kalshi.com/trade-api/v2/events/KXHIGHTSFO-{date_st
 response = requests.get(url)
 series_data = response.json()
 
-# def GetBrackets():
-#     seriesData = requests.get(url)
 
-print(json.dumps(series_data, indent=4))
+# Get brackets/markets for climate predition. Returns a list of tuples for different brackets.
+def GetBrackets():
+    marketsData = requests.get(url).json()["markets"]
+    # print(marketsData)
+
+    brackets = []
+    
+    brackets.append(("<", marketsData[0]["cap_strike"]))
+    for market in marketsData[1:5]:
+        brackets.append((market["floor_strike"], market["cap_strike"]))
+    brackets.append((">", marketsData[5]["floor_strike"]))
+
+    return brackets
+
+
+def main():
+    # print(json.dumps(series_data, indent=4))
+    print(GetBrackets())
+
+if __name__ == "__main__":
+    main()
